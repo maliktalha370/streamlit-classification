c3529501bbd9d19144acce360b95948512834b03
maliktalha370 <maliktalha370@gmail.com>
2024-07-07 12:14:24 +0500

    Adding requirements.txt file along with modified app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 from tensorflow.keras.models import load_model
-
+import efficientnet.keras as efn
 
 # Function to preprocess the image
 def preprocess_image(image, target_size=(224, 224)):
