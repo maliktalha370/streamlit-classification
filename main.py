a454366a8c8d224ffc867c6bfb75576fd2c10985
maliktalha370 <maliktalha370@gmail.com>
2024-07-11 20:07:35 +0500

    Updated main.py: - With the implementation of inferencing. Updated Requirements.txt with gdown

--- main.py
+++ main.py
@@ -1,53 +1,39 @@
-import cv2
-import dlib, os
-import matplotlib.pyplot as plt
-from tqdm import tqdm
+import efficientnet.keras as efn
+import numpy as np
+import time
+import os
 
-# Load the pre-trained facial landmark detector
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+from constants import *
+from PIL import Image
+from tensorflow.keras.models import load_model
 
+# Function to preprocess the image
+def preprocess_image(image, target_size=(224, 224)):
+    # Convert the image to a numpy array
+    image = image.resize(target_size)
+    image_array = np.array(image)
+    image_array = image_array / 255.0  # Normalize image by dividing by 255
+    img_array = np.expand_dims(image_array, axis=0)  # Expand dimensions to match model input
+    return img_array
 
-# Function to detect eyelid closure
-def detect_eyelid_closure(image_name):
-    image = cv2.imread(image_name)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = detector(gray)
-    if len(faces) == 0:
-        height, width, _ = image.shape
-        top = 0
-        left = 0
-        bottom = width
-        right = height
-        faces = [dlib.rectangle(top, left, bottom, right)]
-    for face in faces:
-        landmarks = predictor(gray, face)
+# Function to run inference
+def run_inference(model, img):
+    img_array = preprocess_image(img)
+    prediction = model.predict(img_array)  # Run model prediction
+    # Interpret the prediction
+    return 'Lagophthalmos' if prediction[0][0] < 0.5 else 'Normal'
+def download_model(model_link):
+    os.system(f'gdown {model_link}')
 
-        # Extract eyelid landmarks
-        left_eye_top = landmarks.part(37).y
-        left_eye_bottom = landmarks.part(41).y
-        right_eye_top = landmarks.part(44).y
-        right_eye_bottom = landmarks.part(46).y
+if not os.path.exists(model_path):
+    download_model(model_link)
 
-        left_eye_open = left_eye_bottom - left_eye_top
-        right_eye_open = right_eye_bottom - right_eye_top
-
-        image_cp = cv2.circle(image, (landmarks.part(37).x,landmarks.part(37).y), radius=0, color=(255, 255, 255), thickness=5)
-        image_cp = cv2.circle(image_cp, (landmarks.part(41).x,landmarks.part(41).y), radius=0, color=(255, 255, 255), thickness=5)
-        image_cp = cv2.circle(image_cp, (landmarks.part(44).x, landmarks.part(44).y), radius=0, color=(255, 255, 255),
-                              thickness=5)
-        image_cp = cv2.circle(image_cp, (landmarks.part(46).x, landmarks.part(46).y), radius=0, color=(255, 255, 255),
-                              thickness=5)
-
-
-
-    # cv2.imshow('draw image', image_cp)
-    # cv2.waitKey(0)
-    extracted_name = image_name.split('/')[-1]
-    cv2.imwrite(f'Humans_drawn/{extracted_name}', image_cp)
-
-# detect_eyelid_closure('IMG_4605.jpeg')
-data_dir = 'Humans'
-for image in tqdm(os.listdir(data_dir)):
-    detect_eyelid_closure(data_dir + '/'  + image)
-
+t1 = time.time()
+# Load the model
+model = load_model(model_path)
+uploaded_file = 'demo/IMG_3085.JPG'
+# Display uploaded image
+image = Image.open(uploaded_file)
+# Classify image
+prediction = run_inference(model, image)
+print('Prediction ', prediction)
